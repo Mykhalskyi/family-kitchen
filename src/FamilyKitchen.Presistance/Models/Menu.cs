5a20f0173b71b2f1080c3d4ddfb4d4d2ada18ccf
Ihor Mykhalskyi <ihor.mykhalskyi@gmail.com>
2022-04-23 18:49:18 +0300

    Created menu model.

---
+++ src/FamilyKitchen.Presistance/Models/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyKitchen.Persistance.Models
+{
+    public class Menu
+    {
+        public DateTime Date { get; private set; }
+        public List<Tuple<Dish, int>> Dishes { get; private set; }
+    }
+}
